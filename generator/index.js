b6b043dd2ce9b58bef50eadbc6a5cbd0fa5d7a4f
Noah Klayman <noahklayman@gmail.com>
2018-04-11 12:20:44 -0700

    add typescript support prompt

--- generator/index.js
+++ generator/index.js
@@ -1,15 +1,20 @@
-module.exports = api => {
+module.exports = (api, opts) => {
   api.render('./template');
+  let devDependencies = {
+    'electron-builder': '^20.8.1',
+    'electron-webpack': '^1.13.0',
+    electron: '^1.8.4'
+  };
+  if (opts.useTypescript) {
+    devDependencies['electron-webpack-ts'] = '^2.0.0';
+    devDependencies['typescript'] = '^2.8.1';
+  }
   api.extendPackage({
     scripts: {
       'build:electron': 'vue-cli-service build:electron',
       'serve:electron': 'vue-cli-service serve:electron'
     },
-    devDependencies: {
-      'electron-builder': '^20.8.1',
-      'electron-webpack': '^1.13.0',
-      electron: '^1.8.4'
-    },
+    devDependencies,
     electronWebpack: {
       renderer: {
         sourceDirectory: 'src'
@@ -17,7 +22,6 @@ module.exports = api => {
     },
     build: {
       directories: {
-        app: './',
         output: 'dist_electron'
       },
       files: ['dist/**/*', 'node_modules/**/*', 'package.json']
---
+++ prompts.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'useTypescript',
+    message: 'Do you use TypeScript?',
+    default: false,
+    when: true
+  }
+];
