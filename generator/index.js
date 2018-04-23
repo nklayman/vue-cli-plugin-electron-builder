dbc7f332cba2fddee795f5cea8053872d5ab505f
Noah Klayman <noahklayman@gmail.com>
2018-04-23 13:35:44 -0700

    generator now adds source-map-support to dependencies, fixes #3

--- generator/index.js
+++ generator/index.js
@@ -1,16 +1,20 @@
 module.exports = (api, opts) => {
-  api.render('./template');
+  api.render('./template')
+  let dependencies = {
+    'source-map-support': '^0.5.4'
+  }
   let devDependencies = {
     'electron-builder': '^20.8.1',
     'electron-webpack': '^1.13.0',
     electron: '^1.8.4'
-  };
-  if (opts.useTypescript) devDependencies['electron-webpack-ts'] = '^2.0.0';
+  }
+  if (opts.useTypescript) devDependencies['electron-webpack-ts'] = '^2.0.0'
   api.extendPackage({
     scripts: {
       'build:electron': 'vue-cli-service build:electron',
       'serve:electron': 'vue-cli-service serve:electron'
     },
+    dependencies,
     devDependencies,
     electronWebpack: {
       renderer: {
@@ -23,5 +27,5 @@ module.exports = (api, opts) => {
       },
       files: ['dist/**/*', 'node_modules/**/*', 'package.json']
     }
-  });
-};
+  })
+}
