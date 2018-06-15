ada39cbcc8d8a1df3e065c5697d1e15d27b5543a
Noah Klayman <noahklayman@gmail.com>
2018-06-15 14:59:00 -0700

    format files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const webpack = require('webpack');
-const Config = require('webpack-chain');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+const Config = require('webpack-chain')
 module.exports = (api, options) => {
   api.registerCommand(
     'build:electron',
@@ -15,77 +15,77 @@ module.exports = (api, options) => {
     },
     async () => {
       const buildRenderer = require('@vue/cli-service/lib/commands/build')
-        .build;
-      const fs = require('fs-extra');
-      const builder = require('electron-builder');
-      const rendererConfig = api.resolveChainableWebpackConfig();
+        .build
+      const fs = require('fs-extra')
+      const builder = require('electron-builder')
+      const rendererConfig = api.resolveChainableWebpackConfig()
       const defaultBuildConfig = {
         directories: {
           output: 'dist_electron'
         },
         files: ['dist/**/*', 'node_modules/**/*', 'package.json'],
         extends: null
-      };
-      const userBuildConfig = (function() {
+      }
+      const userBuildConfig = (function () {
         if (
           options.pluginOptions &&
           options.pluginOptions.electronBuilder &&
           options.pluginOptions.electronBuilder.builderConfig
         ) {
-          return options.pluginOptions.electronBuilder.builderConfig;
+          return options.pluginOptions.electronBuilder.builderConfig
         } else {
-          return {};
+          return {}
         }
-      })();
-      const mainConfig = new Config();
+      })()
+      const mainConfig = new Config()
       mainConfig
         .mode('production')
         .devtool('source-map')
         .target('electron-main')
         .node.set('__dirname', false)
-        .set('__filename', false);
-      mainConfig.output.path(api.resolve('./dist')).filename('background.js');
+        .set('__filename', false)
+      mainConfig.output.path(api.resolve('./dist')).filename('background.js')
       mainConfig.plugin('uglify').use(UglifyJSPlugin, [
         {
           parallel: true,
           sourceMap: true
         }
-      ]);
+      ])
       mainConfig
         .plugin('env')
-        .use(webpack.EnvironmentPlugin, [{ NODE_ENV: 'production' }]);
-      mainConfig.entry('background').add(api.resolve('./src/background.js'));
+        .use(webpack.EnvironmentPlugin, [{ NODE_ENV: 'production' }])
+      mainConfig.entry('background').add(api.resolve('./src/background.js'))
 
-      console.log('Bundling render process:');
-      rendererConfig.target('electron-renderer').output.publicPath('./');
-      await buildRenderer({ _: [] }, api, options, rendererConfig);
+      console.log('Bundling render process:')
+      rendererConfig.target('electron-renderer').output.publicPath('./')
+      await buildRenderer({ _: [] }, api, options, rendererConfig)
       if (fs.existsSync(api.resolve('./dist/fonts'))) {
-        fs.mkdirSync(api.resolve('./dist/css/fonts'));
+        fs.mkdirSync(api.resolve('./dist/css/fonts'))
         fs.copySync(
           api.resolve('./dist/fonts'),
           api.resolve('./dist/css/fonts')
-        );
+        )
       }
-      const bundle = webpack(mainConfig.toConfig());
-      console.log('Bundling main process:\n');
+      const bundle = webpack(mainConfig.toConfig())
+      console.log('Bundling main process:\n')
       bundle.run((err, stats) => {
         if (err) {
-          console.error(err.stack || err);
+          console.error(err.stack || err)
           if (err.details) {
-            console.error(err.details);
+            console.error(err.details)
           }
-          process.exit(1);
+          process.exit(1)
         }
 
-        const info = stats.toJson();
+        const info = stats.toJson()
 
         if (stats.hasErrors()) {
-          console.error(info.errors);
-          process.exit(1);
+          console.error(info.errors)
+          process.exit(1)
         }
 
         if (stats.hasWarnings()) {
-          console.warn(info.warnings);
+          console.warn(info.warnings)
         }
 
         console.log(
@@ -93,9 +93,9 @@ module.exports = (api, options) => {
             chunks: false,
             colors: true
           })
-        );
+        )
 
-        console.log('\nBuilding app with electron-builder:\n');
+        console.log('\nBuilding app with electron-builder:\n')
 
         builder
           .build({
@@ -106,15 +106,15 @@ module.exports = (api, options) => {
           })
           .then(() => {
             // handle result
-            console.log('\nBuild complete!\n');
+            console.log('\nBuild complete!\n')
           })
           .catch(err => {
             // handle error
-            throw err;
-          });
-      });
+            throw err
+          })
+      })
     }
-  );
+  )
   api.registerCommand(
     'serve:electron',
     {
@@ -123,48 +123,48 @@ module.exports = (api, options) => {
       details: `See https://github.com/nklayman/vue-cli-plugin-electron-builder for more details.`
     },
     () => {
-      const execa = require('execa');
-      const mainConfig = new Config();
+      const execa = require('execa')
+      const mainConfig = new Config()
       mainConfig
         .mode('development')
         .devtool('source-map')
         .target('electron-main')
         .node.set('__dirname', false)
-        .set('__filename', false);
+        .set('__filename', false)
       mainConfig.output
         .path(api.resolve('./dist_electron'))
-        .filename('background.js');
+        .filename('background.js')
       mainConfig.plugin('uglify').use(UglifyJSPlugin, [
         {
           parallel: true,
           sourceMap: true
         }
-      ]);
+      ])
       mainConfig
         .plugin('env')
-        .use(webpack.EnvironmentPlugin, [{ NODE_ENV: 'development' }]);
-      mainConfig.entry('background').add(api.resolve('./src/background.js'));
-      const bundle = webpack(mainConfig.toConfig());
+        .use(webpack.EnvironmentPlugin, [{ NODE_ENV: 'development' }])
+      mainConfig.entry('background').add(api.resolve('./src/background.js'))
+      const bundle = webpack(mainConfig.toConfig())
 
-      console.log('Bundling main process:\n');
+      console.log('Bundling main process:\n')
       bundle.run((err, stats) => {
         if (err) {
-          console.error(err.stack || err);
+          console.error(err.stack || err)
           if (err.details) {
-            console.error(err.details);
+            console.error(err.details)
           }
-          process.exit(1);
+          process.exit(1)
         }
 
-        const info = stats.toJson();
+        const info = stats.toJson()
 
         if (stats.hasErrors()) {
-          console.error(info.errors);
-          process.exit(1);
+          console.error(info.errors)
+          process.exit(1)
         }
 
         if (stats.hasWarnings()) {
-          console.warn(info.warnings);
+          console.warn(info.warnings)
         }
 
         console.log(
@@ -172,12 +172,12 @@ module.exports = (api, options) => {
             chunks: false,
             colors: true
           })
-        );
+        )
 
-        console.log('\nStarting development server:\n');
+        console.log('\nStarting development server:\n')
 
         api.service.run('serve').then(server => {
-          console.log('\nLaunching Electron...\n');
+          console.log('\nLaunching Electron...\n')
           const child = execa(
             './node_modules/.bin/electron dist_electron/background.js',
             {
@@ -187,16 +187,16 @@ module.exports = (api, options) => {
                 WEBPACK_DEV_SERVER_URL: server.url
               }
             }
-          );
+          )
           child.on('exit', () => {
-            process.exit(0);
-          });
-        });
-      });
+            process.exit(0)
+          })
+        })
+      })
     }
-  );
-};
+  )
+}
 module.exports.defaultModes = {
   'build:electron': 'production',
   'serve:electron': 'development'
-};
+}
