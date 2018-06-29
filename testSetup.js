72542b6f62801b9fc635efba68669557d184978b
Noah Klayman <noahklayman@gmail.com>
2018-06-28 18:20:57 -0700

    update method of clearing test projects

--- testSetup.js
+++ testSetup.js
@@ -1,5 +1,14 @@
 const lnk = require('lnk')
 const fs = require('fs-extra')
+const rimraf = require('rimraf')
+// Prevent full and unnecessary project creation
 process.env.VUE_CLI_TEST = true
+// Link ./ to node_modules/vcp-electron-builder to that require.resolve(vcp-electron-builder) returns ./
 fs.removeSync('./node_modules/vue-cli-plugin-electron-builder')
 lnk.sync(['./'], './node_modules/vue-cli-plugin-electron-builder')
+// Create project dir if it doesn't exist, clear it if it does
+if (fs.existsSync('./__tests__/projects')) {
+  rimraf.sync('./__tests__/projects/*')
+} else {
+  fs.mkdirSync('./__tests__/projects')
+}
