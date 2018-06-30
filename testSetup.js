79a34877707b40053d81c7810b7d3af22f6aa269
Noah Klayman <noahklayman@gmail.com>
2018-06-30 13:15:56 -0700

    allow tests to run in parallel

--- testSetup.js
+++ testSetup.js
@@ -1,14 +1,13 @@
 const lnk = require('lnk')
 const fs = require('fs-extra')
-const rimraf = require('rimraf')
 // Prevent full and unnecessary project creation
 process.env.VUE_CLI_TEST = true
 // Link ./ to node_modules/vcp-electron-builder to that require.resolve(vcp-electron-builder) returns ./
-fs.removeSync('./node_modules/vue-cli-plugin-electron-builder')
-lnk.sync(['./'], './node_modules/vue-cli-plugin-electron-builder')
-// Create project dir if it doesn't exist, clear it if it does
-if (fs.existsSync('./__tests__/projects')) {
-  rimraf.sync('./__tests__/projects/*')
-} else {
+// fs.removeSync('./node_modules/vue-cli-plugin-electron-builder')
+if (!fs.existsSync('./node_modules/vue-cli-plugin-electron-builder')) {
+  lnk.sync(['./'], './node_modules/vue-cli-plugin-electron-builder')
+}
+// Create project dir if it doesn't exist
+if (!fs.existsSync('./__tests__/projects')) {
   fs.mkdirSync('./__tests__/projects')
 }
