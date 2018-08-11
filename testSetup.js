399dcaab658e94adbf985cab8963816a3f3dc717
Noah Klayman <noahklayman@gmail.com>
2018-08-11 12:32:59 -0700

    fix(tests): catch link error

--- testSetup.js
+++ testSetup.js
@@ -5,7 +5,11 @@ process.env.VUE_CLI_TEST = true
 // Link ./ to node_modules/vcp-electron-builder to that require.resolve(vcp-electron-builder) returns ./
 // fs.removeSync('./node_modules/vue-cli-plugin-electron-builder')
 if (!fs.existsSync('./node_modules/vue-cli-plugin-electron-builder')) {
-  lnk.sync(['./'], './node_modules/vue-cli-plugin-electron-builder')
+  try {
+    lnk.sync(['./'], './node_modules/vue-cli-plugin-electron-builder')
+  } catch (err) {
+    if (err.code !== 'EEXIST') console.error(err)
+  }
 }
 // Create project dir if it doesn't exist
 if (!fs.existsSync('./__tests__/projects')) {
