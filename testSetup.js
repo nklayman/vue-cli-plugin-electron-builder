82cf8324548212dc0cc1f29c8e63f265cfa39550
Noah Klayman <noahklayman@gmail.com>
2018-08-17 12:58:06 -0700

    [skip ci] tests(fix): prevent EEXIST error on async tests

--- testSetup.js
+++ testSetup.js
@@ -2,8 +2,7 @@ const lnk = require('lnk')
 const fs = require('fs-extra')
 // Prevent full and unnecessary project creation
 process.env.VUE_CLI_TEST = true
-// Link ./ to node_modules/vcp-electron-builder to that require.resolve(vcp-electron-builder) returns ./
-// fs.removeSync('./node_modules/vue-cli-plugin-electron-builder')
+// Link ./ to node_modules/vcp-electron-builder so that require.resolve(vcp-electron-builder) returns ./
 if (!fs.existsSync('./node_modules/vue-cli-plugin-electron-builder')) {
   try {
     lnk.sync(['./'], './node_modules/vue-cli-plugin-electron-builder')
@@ -13,5 +12,9 @@ if (!fs.existsSync('./node_modules/vue-cli-plugin-electron-builder')) {
 }
 // Create project dir if it doesn't exist
 if (!fs.existsSync('./__tests__/projects')) {
-  fs.mkdirSync('./__tests__/projects')
+  try {
+    fs.mkdirSync('./__tests__/projects')
+  } catch (err) {
+    if (err.code !== 'EEXIST') console.error(err)
+  }
 }
