d4d55c67813b2790fe1b3d3e415e36b58e9c38ff
Noah Klayman <noahklayman@gmail.com>
2018-08-24 11:07:47 -0700

    feat(generator): Generator only adds missing features

--- __tests__/generator.spec.js
+++ __tests__/generator.spec.js
@@ -18,11 +18,6 @@ const mockApi = {
   hasPlugin: jest.fn()
 }
 beforeEach(() => {
-  // Reset mock status
-  jest.clearAllMocks()
-})
-
-test('extends gitignore if it exists', () => {
   fs.readFileSync.mockImplementation((path, encoding) => {
     // Check that utf8 encoding is set
     expect(encoding).toBe('utf8')
@@ -32,59 +27,259 @@ test('extends gitignore if it exists', () => {
     // return mock content
     return 'existing_content'
   })
-  // Mock existence of .gitignore
-  fs.existsSync = jest.fn(path => path === 'apiResolve_./.gitignore')
-  // Run the generator with mock api
-  generator(mockApi)
-  // Run the onCreateComplete callback
-  completionCb()
-  // New .gitignore should have been written
-  expect(fs.writeFileSync).toBeCalledWith(
-    'apiResolve_./.gitignore',
-    'existing_content\n#Electron-builder output\n/dist_electron'
-  )
-})
-test("doesn't modify .gitignore if it doesn't exist", () => {
-  // Mock lack of .gitignore
-  fs.existsSync = jest.fn(path => !(path === 'apiResolve_./.gitignore'))
-  // Run the generator with mock api
-  generator(mockApi)
-  // Run the onCreateComplete callback
-  completionCb()
-  // New .gitignore should not have been read from or written
-  expect(fs.writeFileSync).not.toBeCalledWith(
-    'apiResolve_./.gitignore',
-    'existing_content\n#Electron-builder output\n/dist_electron'
-  )
-  expect(fs.readFileSync).not.toBeCalledWith('apiResolve_./.gitignore', 'utf8')
-  // Only index should have been written
-  expect(fs.writeFileSync).toHaveBeenCalledTimes(1)
+  // Reset mock status
+  jest.clearAllMocks()
 })
 
-test('Adds electron-builder install-app-deps to postInstall', () => {
-  generator(mockApi)
-  completionCb()
-  expect(pkg.scripts.postinstall).toBe('electron-builder install-app-deps')
-})
-
-test('Adds on to existing postinstall script instead of replacing', () => {
-  fs.readFileSync.mockImplementation((path, encoding) => {
-    // Check that utf8 encoding is set
-    expect(encoding).toBe('utf8')
-    // Mock existing postinstall script in app's package.json
-    if (path === 'apiResolve_./package.json') {
-      return JSON.stringify({
-        scripts: { postinstall: 'existingTask' }
-      })
-    }
-    // return mock content
-    return 'existing_content'
+describe('.gitignore', () => {
+  test('extends gitignore if it exists', () => {
+    // Mock existence of .gitignore
+    fs.existsSync = jest.fn(path => path === 'apiResolve_./.gitignore')
+    // Run the generator with mock api
+    generator(mockApi)
+    // Run the onCreateComplete callback
+    completionCb()
+    // New .gitignore should have been written
+    expect(fs.writeFileSync).toBeCalledWith(
+      'apiResolve_./.gitignore',
+      'existing_content\n#Electron-builder output\n/dist_electron'
+    )
   })
 
-  generator(mockApi)
-  completionCb()
+  test("doesn't modify .gitignore if it doesn't exist", () => {
+    // Mock lack of .gitignore
+    fs.existsSync = jest.fn(path => !(path === 'apiResolve_./.gitignore'))
+    // Run the generator with mock api
+    generator(mockApi)
+    // Run the onCreateComplete callback
+    completionCb()
+    // New .gitignore should not have been read from or written
+    expect(fs.writeFileSync).not.toBeCalledWith(
+      'apiResolve_./.gitignore',
+      expect.any(String)
+    )
+    expect(fs.readFileSync).not.toBeCalledWith(
+      'apiResolve_./.gitignore',
+      expect.any(String)
+    )
+    // Only index and background should have been written
+    expect(fs.writeFileSync).toHaveBeenCalledTimes(2)
+  })
 
-  expect(pkg.scripts.postinstall).toBe(
-    'existingTask && electron-builder install-app-deps'
+  test.each(['#Electron-builder output', '/dist_electron'])(
+    'does not modify .gitignore if it has "%s"',
+    existing => {
+      fs.readFileSync.mockImplementation((path, encoding) => {
+        // Check that utf8 encoding is set
+        expect(encoding).toBe('utf8')
+        if (path === 'apiResolve_./package.json') {
+          return JSON.stringify({ scripts: {} })
+        }
+        // return mock content
+        return `ignore-this\n${existing}\nignore-that`
+      })
+
+      // Mock existence of .gitignore
+      fs.existsSync = jest.fn(path => path === 'apiResolve_./.gitignore')
+      // Run the generator with mock api
+      generator(mockApi)
+      // Run the onCreateComplete callback
+      completionCb()
+      // New .gitignore should not have been written
+      expect(fs.writeFileSync).not.toBeCalledWith(
+        'apiResolve_./.gitignore',
+        expect.any(String)
+      )
+      // Only index should have been written
+      expect(fs.writeFileSync).toHaveBeenCalledTimes(1)
+    }
   )
+})
+
+describe('index.html', () => {
+  test.each([
+    // None
+    '',
+    // Node_modules path
+    `    <% if (VUE_APP_NODE_MODULES_PATH !== "false") { %><script>require('module').globalPaths.push('<%= VUE_APP_NODE_MODULES_PATH %>')</script><% } %>\n`,
+    // Base URL
+    `    <% if (BASE_URL === './') { %><base href="app://./" /><% } %>\n`,
+    // Both
+    `    <% if (BASE_URL === './') { %><base href="app://./" /><% } %>
+    <% if (VUE_APP_NODE_MODULES_PATH !== "false") { %><script>require('module').globalPaths.push('<%= VUE_APP_NODE_MODULES_PATH %>')</script><% } %>\n`
+  ])('Only add missing tags to index.html', existing => {
+    // Disable .gitignore modification
+    fs.existsSync.mockReturnValueOnce(false)
+    fs.readFileSync.mockImplementation((path, encoding) => {
+      // Check that utf8 encoding is set
+      expect(encoding).toBe('utf8')
+      if (path === 'apiResolve_./package.json') {
+        return JSON.stringify({ scripts: {} })
+      }
+      // return mock content
+      return `<head>\n${existing}  </head>`
+    })
+
+    // Run the generator with mock api
+    generator(mockApi)
+    // Run the onCreateComplete callback
+    completionCb()
+
+    const index = fs.writeFileSync.mock.calls[0][1]
+    // Opening and closing tags still exist
+    expect(index.match(/^\s*?<head.*?>\s*?$/gm).length).toBe(1)
+    expect(index.match(/^\s*?<\/head.*?>\s*?$/gm).length).toBe(1)
+    // Each tag exists once
+    expect(
+      index.match(
+        // Base URL
+        /<% if \(BASE_URL === '\.\/'\) { %><base href="app:\/\/\.\/" \/><% } %>/g
+      ).length
+    ).toBe(1)
+    expect(
+      index.match(
+        // Node_modules path
+        /<% if \(VUE_APP_NODE_MODULES_PATH !== "false"\) { %><script>require\('module'\)\.globalPaths\.push\('<%= VUE_APP_NODE_MODULES_PATH %>'\)<\/script><% } %>/g
+      ).length
+    ).toBe(1)
+  })
+})
+
+describe('background.js', () => {
+  test.each([false, true])(
+    'Background file is not added if it exists',
+    usesTS => {
+      const file = `src/background.${usesTS ? 'ts' : 'js'}`
+      // Mock having typescript
+      mockApi.hasPlugin.mockImplementationOnce(
+        plugin => plugin === 'typescript' && usesTS
+      )
+      // Mock existence of background file
+      fs.existsSync.mockImplementation(path => path === `apiResolve_./${file}`)
+      generator(mockApi)
+      completionCb()
+      expect(mockApi.render).not.toBeCalled()
+    }
+  )
+
+  test.each([
+    '(process.env.WEBPACK_DEV_SERVER_URL as string)',
+    'let mainWindow: any'
+  ])('Types are not re-added for "%s"', background => {
+    // Mock typescript existence
+    mockApi.hasPlugin.mockImplementationOnce(plugin => plugin === 'typescript')
+    // Mock background file existence
+    fs.existsSync.mockImplementation(
+      file => file === 'apiResolve_./src/background.js'
+    )
+    fs.readFileSync.mockImplementation((path, encoding) => {
+      // Check that utf8 encoding is set
+      expect(encoding).toBe('utf8')
+      // Mock existing postinstall script in app's package.json
+      if (path === 'apiResolve_./package.json') {
+        return JSON.stringify({
+          scripts: {}
+        })
+      }
+      // return mock content
+      return background
+    })
+    generator(mockApi)
+    completionCb()
+    expect(fs.writeFileSync).toBeCalledWith(
+      'apiResolve_./src/background.ts',
+      background
+    )
+  })
+
+  test.each([false, true])(
+    'Background has node module path added if it does not exist',
+    usesTS => {
+      const file = `src/background.${usesTS ? 'ts' : 'js'}`
+      // Mock having typescript
+      mockApi.hasPlugin.mockImplementationOnce(
+        plugin => plugin === 'typescript' && usesTS
+      )
+      // Mock not having node path added
+      fs.readFileSync.mockImplementation((path, encoding) => {
+        // Check that utf8 encoding is set
+        expect(encoding).toBe('utf8')
+        if (path === 'apiResolve_./package.json') {
+          return JSON.stringify({ scripts: {} })
+        }
+        // return mock content
+        return `const isDevelopment = process.env.NODE_ENV !== 'production'
+existing_content`
+      })
+      // Mock existence of background file
+      fs.existsSync.mockImplementation(path => path === `apiResolve_./${file}`)
+      generator(mockApi)
+      completionCb()
+      expect(mockApi.render).not.toBeCalled()
+      expect(fs.writeFileSync).toBeCalledWith(
+        `apiResolve_./${file}`,
+        `const isDevelopment = process.env.NODE_ENV !== 'production'
+if (isDevelopment) {
+  // Don't load any native (external) modules until the following line is run:
+  require('module').globalPaths.push(process.env.NODE_MODULES_PATH)
+}
+
+existing_content`
+      )
+    }
+  )
+})
+
+describe('package.json', () => {
+  test('Adds electron-builder install-app-deps to postInstall', () => {
+    generator(mockApi)
+    completionCb()
+    expect(pkg.scripts.postinstall).toBe('electron-builder install-app-deps')
+  })
+
+  test('Adds on to existing postinstall script instead of replacing', () => {
+    fs.readFileSync.mockImplementation((path, encoding) => {
+      // Check that utf8 encoding is set
+      expect(encoding).toBe('utf8')
+      // Mock existing postinstall script in app's package.json
+      if (path === 'apiResolve_./package.json') {
+        return JSON.stringify({
+          scripts: { postinstall: 'existingTask' }
+        })
+      }
+      // return mock content
+      return 'existing_content'
+    })
+
+    generator(mockApi)
+    completionCb()
+
+    expect(pkg.scripts.postinstall).toBe(
+      'existingTask && electron-builder install-app-deps'
+    )
+  })
+
+  test('Does not add install-app-deps multiple times', () => {
+    fs.readFileSync.mockImplementation((path, encoding) => {
+      // Check that utf8 encoding is set
+      expect(encoding).toBe('utf8')
+      // Mock existing postinstall script in app's package.json
+      if (path === 'apiResolve_./package.json') {
+        return JSON.stringify({
+          scripts: {
+            postinstall: 'existingTask && electron-builder install-app-deps'
+          }
+        })
+      }
+      // return mock content
+      return 'existing_content'
+    })
+
+    generator(mockApi)
+    completionCb()
+
+    expect(pkg.scripts.postinstall).toBe(
+      'existingTask && electron-builder install-app-deps'
+    )
+  })
 })
--- generator/index.js
+++ generator/index.js
@@ -1,40 +1,103 @@
 const fs = require('fs')
 
 module.exports = api => {
-  api.render('./template')
+  const usesTS = api.hasPlugin('typescript')
+  const hasBackground =
+    fs.existsSync(api.resolve(`./src/background.ts`)) ||
+    fs.existsSync(api.resolve(`./src/background.js`))
+  if (!hasBackground) {
+    // If user does not have a background file so it should be created
+    api.render('./template')
+  }
   api.onCreateComplete(() => {
     //   Read existing index.html and .gitignore
     let index = fs.readFileSync(api.resolve('./public/index.html'), 'utf8')
-    //   Add base element inside <head> tag
-    index = index.replace(
-      /^\s*?<head.*?>\s*?$/m,
-      `<head>\n    <% if (BASE_URL === './') { %><base href="app://./" /><% } %>
-    <% if (VUE_APP_NODE_MODULES_PATH !== "false") { %><script>require('module').globalPaths.push('<%= VUE_APP_NODE_MODULES_PATH %>')</script><% } %>`
-    )
+
+    // List of tags that need to be included in index.html
+    const tags = [
+      `<% if (BASE_URL === './') { %><base href="app://./" /><% } %>`,
+      `<% if (VUE_APP_NODE_MODULES_PATH !== "false") { %><script>require('module').globalPaths.push('<%= VUE_APP_NODE_MODULES_PATH %>')</script><% } %>`
+    ]
+    let elements = '  <head>'
+    tags.forEach(tag => {
+      if (index.indexOf(tag) === -1) {
+        elements += `\n    ${tag}`
+      }
+    })
+    //   Add extra elements inside <head> tag
+    index = index.replace(/^\s*?<head.*?>\s*?$/m, elements)
     //   Write updated index.html
     fs.writeFileSync(api.resolve('./public/index.html'), index)
 
     // Update .gitignore if it exists
     if (fs.existsSync(api.resolve('./.gitignore'))) {
       let gitignore = fs.readFileSync(api.resolve('./.gitignore'), 'utf8')
-      //   Add /dist_electron to gitignore
-      gitignore = gitignore + '\n#Electron-builder output\n/dist_electron'
-      fs.writeFileSync(api.resolve('./.gitignore'), gitignore)
+      if (!gitignore.match(/(#Electron-builder output|\/dist_electron)/)) {
+        //   Add /dist_electron to gitignore if it doesn't exist already
+        gitignore = gitignore + '\n#Electron-builder output\n/dist_electron'
+        fs.writeFileSync(api.resolve('./.gitignore'), gitignore)
+      }
     }
 
-    if (api.hasPlugin('typescript')) {
+    if (hasBackground) {
+      // Find background file
+      let background
+      if (fs.existsSync(api.resolve('./src/background.js'))) {
+        background = fs.readFileSync(api.resolve('./src/background.js'), 'utf8')
+      } else if (fs.existsSync(api.resolve('./src/background.ts'))) {
+        background = fs.readFileSync(api.resolve('./src/background.ts'), 'utf8')
+      } else {
+        // Exit if background file cannot be found
+        return
+      }
+
+      if (
+        // Only change file if it is missing module path addition
+        !background.match(
+          /require\('module'\)\.globalPaths\.push\(process\.env\.NODE_MODULES_PATH\)/
+        )
+      ) {
+        // Remove old isDevelopment const
+        background = background.replace(
+          "const isDevelopment = process.env.NODE_ENV !== 'production'",
+          ''
+        )
+        // Add node_modules path and isDevelopment const
+        background =
+          `const isDevelopment = process.env.NODE_ENV !== 'production'
+if (isDevelopment) {
+  // Don't load any native (external) modules until the following line is run:
+  require('module').globalPaths.push(process.env.NODE_MODULES_PATH)
+}\n` + background
+
+        // Write new background
+        fs.writeFileSync(
+          api.resolve(`./src/background.${usesTS ? 'ts' : 'js'}`),
+          background
+        )
+      }
+    }
+
+    if (usesTS) {
       let background
       if (fs.existsSync(api.resolve('./src/background.js'))) {
         background = fs.readFileSync(api.resolve('./src/background.js'), 'utf8')
         fs.unlinkSync(api.resolve('./src/background.js'))
-      } else {
+      } else if (fs.existsSync(api.resolve('./src/background.ts'))) {
         background = fs.readFileSync(api.resolve('./src/background.ts'), 'utf8')
+      } else {
+        // Exit if background file cannot be found
+        return
       }
       background = background.replace(
-        'process.env.WEBPACK_DEV_SERVER_URL',
-        'process.env.WEBPACK_DEV_SERVER_URL as string'
+        // Add types if they don't exist
+        /process\.env\.WEBPACK_DEV_SERVER_URL\s*?\)$/m,
+        'process.env.WEBPACK_DEV_SERVER_URL as string)'
       )
-      background = background.replace('let mainWindow', 'let mainWindow: any')
+      background = background.replace(
+        /let mainWindow\s*?$/m,
+        'let mainWindow: any'
+      )
       fs.writeFileSync(api.resolve('./src/background.ts'), background)
     }
   })
@@ -43,12 +106,16 @@ module.exports = api => {
   let postinstallScript
   let pkg = fs.readFileSync(api.resolve('./package.json'), 'utf8')
   pkg = JSON.parse(pkg)
+  // Add on to existing script if it exists
   if (pkg.scripts && pkg.scripts.postinstall) {
-    // Add on to existing script if it exists
-    postinstallScript = `${
-      // Existing script
-      pkg.scripts.postinstall
-    } && electron-builder install-app-deps`
+    // Don't re-add script
+    if (!pkg.scripts.postinstall.match('electron-builder install-app-deps')) {
+      postinstallScript =
+        pkg.scripts.postinstall + ' && electron-builder install-app-deps'
+    } else {
+      // electron-builder install-app-deps already exists
+      postinstallScript = pkg.scripts.postinstall
+    }
   } else {
     // Create new postinstall script
     postinstallScript = 'electron-builder install-app-deps'
