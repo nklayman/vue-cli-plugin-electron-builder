9b4b676d78d0c6c75c2771e11fa1e4d82261f89e
Noah Klayman <noahklayman@gmail.com>
2018-10-16 13:58:14 -0700

    [skip ci] fix(tests): serve and serve-ts overlapping

--- __tests__/serveTS.spec.js
+++ __tests__/serveTS.spec.js
@@ -1,6 +1,10 @@
 jest.setTimeout(100000)
 
+const sleep = t => new Promise(resolve => setTimeout(resolve, t))
+
 const runTests = require('./serve.helper.js').runTests
 test('electron:serve-ts', async () => {
+  // Prevent serve/serve-ts tests from overlapping
+  await sleep(2000)
   await runTests(true)
 })
