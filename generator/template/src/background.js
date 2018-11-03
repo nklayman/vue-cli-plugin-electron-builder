8dc5bbabeca7185abd0ee93864e814ee313804fa
Noah Klayman <noahklayman@gmail.com>
2018-11-02 17:42:00 -0700

    fix(generator/background): use app protocol for index.html, fixes #129

--- generator/template/src/background.js
+++ generator/template/src/background.js
@@ -24,7 +24,7 @@ function createWindow () {
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    win.loadFile('index.html')
+    win.loadURL('app://./index.html')
   }
 
   win.on('closed', () => {
