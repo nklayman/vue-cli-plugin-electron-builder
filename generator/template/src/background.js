86a89a6df07dfa2ad7773bc51dd0cb73b0953eca
Noah Klayman <noahklayman@gmail.com>
2018-11-06 14:38:57 -0800

    fix(testWithSpectron): DOM content not loading

--- generator/template/src/background.js
+++ generator/template/src/background.js
@@ -17,7 +17,7 @@ function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 })
 
-  if (isDevelopment) {
+  if (isDevelopment || process.env.IS_TEST) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
