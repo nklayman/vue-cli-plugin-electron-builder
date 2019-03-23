6cd967612e92fd75d63fce81a57acd5c71395b15
Keith Pesavento <kpes@users.noreply.github.com>
2019-03-23 15:44:25 -0400

    Removing now unused constant

--- generator/templates/base/src/background.js
+++ generator/templates/base/src/background.js
@@ -5,7 +5,6 @@ import {
   createProtocol,
   installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
