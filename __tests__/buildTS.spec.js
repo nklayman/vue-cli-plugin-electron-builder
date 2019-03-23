14efe975195b2f70a2aff23d214bbe26faa73b19
Noah Klayman <noahklayman@gmail.com>
2019-03-22 17:01:01 -0700

    tests(build): Prevent build/build-ts from overlapping

--- __tests__/buildTS.spec.js
+++ __tests__/buildTS.spec.js
@@ -1,7 +1,11 @@
 jest.setTimeout(600000)
 
+const sleep = t => new Promise(resolve => setTimeout(resolve, t))
+
 const runTests = require('./build.helper.js').runTests
 
 test('electron:build-ts', async () => {
+  // Prevent build/build-ts tests from overlapping
+  await sleep(2000)
   await runTests(true)
 })
