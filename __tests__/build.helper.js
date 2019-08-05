c77ebacf45f975c967ed5defe1f10966712736a3
Noah Klayman <noahklayman@gmail.com>
2019-08-05 15:16:16 -0700

    tests(build): don't build for linux on windows

--- __tests__/build.helper.js
+++ __tests__/build.helper.js
@@ -13,11 +13,7 @@ const runTests = useTS =>
     const projectPath = p =>
       path.join(process.cwd(), '__tests__/projects/' + projectName, p)
 
-    await project.run(
-      `vue-cli-service electron:build --x64 --dir --linux${
-        isWin ? ' --windows' : ''
-      }`
-    )
+    await project.run('vue-cli-service electron:build --x64 --dir')
     //   Ensure /dist is not modified
     expect(project.has('dist')).toBe(false)
     //   Ensure build successfully outputted files
@@ -30,10 +26,11 @@ const runTests = useTS =>
       expect(project.has(`dist_electron/win-unpacked/${projectName}.exe`)).toBe(
         true
       )
+    } else {
+      expect(project.has(`dist_electron/linux-unpacked/${projectName}`)).toBe(
+        true
+      )
     }
-    expect(project.has(`dist_electron/linux-unpacked/${projectName}`)).toBe(
-      true
-    )
     //   Ensure that setup files were not created
     expect(project.has(`dist_electron/${projectName} Setup 0.1.0.exe`)).toBe(
       false
