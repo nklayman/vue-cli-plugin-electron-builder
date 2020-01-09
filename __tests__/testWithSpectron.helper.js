8e60e6bbe7756798fcf71fd1ab65f82e5e5f49cc
Noah Klayman <noahklayman@gmail.com>
2020-01-09 12:49:41 -0800

    tests(testWithSpectron): fix spectron not launching

--- __tests__/testWithSpectron.helper.js
+++ __tests__/testWithSpectron.helper.js
@@ -1,6 +1,7 @@
 const createProject = require('./createProject.helper.js')
 const { readFile, writeFile } = require('fs-extra')
 const { join } = require('path')
+const execa = require('execa')
 
 module.exports = async testRunner => {
   const plugins = {}
@@ -14,13 +15,22 @@ module.exports = async testRunner => {
   await project.rm('tests/unit/example.spec.js')
 
   // Copy electron test
-  const testFile = (await readFile(
-    `./generator/templates/tests-${testRunner}/tests/unit/electron.spec.js`,
-    'utf8'
-  ))
+  const testFile = (
+    await readFile(
+      `./generator/templates/tests-${testRunner}/tests/unit/electron.spec.js`,
+      'utf8'
+    )
+  )
     // Fix some unknown error
     .replace('testWithSpectron()', 'testWithSpectron({ mode: "production" })')
   await writeFile(join(project.dir, 'tests/unit/electron.spec.js'), testFile)
 
-  await project.run('vue-cli-service test:unit')
+  await execa(
+    require.resolve('@vue/cli-service/bin/vue-cli-service'),
+    ['test:unit'],
+    {
+      cwd: project.dir,
+      extendEnv: false
+    }
+  )
 }
