dd602606bc42bc91308967b014e6877fbe11a7bc
Noah Klayman <noahklayman@gmail.com>
2020-06-01 16:13:58 -0700

    fix(tests): spectron-jest not exiting on windows

--- __tests__/testWithSpectron.helper.js
+++ __tests__/testWithSpectron.helper.js
@@ -1,5 +1,5 @@
 const createProject = require('./createProject.helper.js')
-const { readFile, writeFile } = require('fs-extra')
+const { copyFile, writeFile } = require('fs-extra')
 const { join } = require('path')
 const execa = require('execa')
 
@@ -15,15 +15,14 @@ module.exports = async testRunner => {
   await project.rm('tests/unit/example.spec.js')
 
   // Copy electron test
-  const testFile = (
-    await readFile(
-      `./generator/templates/tests-${testRunner}/tests/unit/electron.spec.js`,
-      'utf8'
-    )
-  )
-    // Fix some unknown error
-    .replace('testWithSpectron()', 'testWithSpectron({ mode: "production" })')
-  await writeFile(join(project.dir, 'tests/unit/electron.spec.js'), testFile)
+  await copyFile(`./generator/templates/tests-${testRunner}/tests/unit/electron.spec.js`, join(project.dir, 'tests/unit/electron.spec.js'))
+
+  // If there is not a second test file, the `serve:electron --headless` process cannot be killed on Windows
+  // One of the weirdest bugs I've ever seen
+  // Without this, tests will fail on Windows
+  if (testRunner === 'jest') {
+    await writeFile(join(project.dir, 'tests/unit/second.spec.js'), 'test("extra", () => {})')
+  }
 
   await execa(
     require.resolve('@vue/cli-service/bin/vue-cli-service'),
