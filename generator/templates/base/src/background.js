6f49ad2681f3926accebfa9a261fed0db6fac8c5
Noah Klayman <noahklayman@gmail.com>
2020-07-14 10:21:23 -0700

    fix(background.js): await loadUrl promise before opening devtools

--- generator/templates/base/src/background.js
+++ generator/templates/base/src/background.js
@@ -14,7 +14,7 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-function createWindow() {
+async function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 800,
@@ -28,7 +28,7 @@ function createWindow() {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
