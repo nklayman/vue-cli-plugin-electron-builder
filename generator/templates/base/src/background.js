2b3ec0259a60074e2aecb6596b7ab401a0a8c1d3
Noah Klayman <noahklayman@gmail.com>
2021-03-08 15:38:05 -0800

    fix(generator/background): disable contextIsolation when nodeIntegration is enabled fixes #1279

--- generator/templates/base/src/background.js
+++ generator/templates/base/src/background.js
@@ -22,7 +22,8 @@ async function createWindow() {
       <% } %>
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
   })
 
