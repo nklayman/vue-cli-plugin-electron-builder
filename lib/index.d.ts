e5d7d49c2a5e0ada1d75e5038fc69c0234276d65
Noah Klayman <noahklayman@gmail.com>
2022-01-02 14:18:40 -0800

    chore: remove legacy installVueDevtools

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
 import { Protocol } from 'electron'
 
 export function createProtocol(scheme: string, customProtocol?: Protocol): void
-export function installVueDevtools(forceDownload?: boolean): Promise<string>
