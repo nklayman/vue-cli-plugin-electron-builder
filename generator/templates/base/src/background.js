823e5bd2aafb4a4c6dbbb1253f70319cd155a6ca
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-02-19 12:49:38 +0100

    Use double not to supress type warnings

--- generator/templates/base/src/background.js
+++ generator/templates/base/src/background.js
@@ -19,7 +19,7 @@ async function createWindow() {
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      nodeIntegration: !!process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
   })
